9074a084713c82f48481f0fde1921f4f3d823e9e
DataVizGenius <168847652+DataVizGenius@users.noreply.github.com>
2024-05-08 22:33:40 +0530

    Add files via upload

---
+++ test002.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # my first code
+# cbambdff 
+# ## check now
+# savjhdhgqhd
+
+# In[2]:
+
+
+print ('this is ankita')
+
+
+# In[3]:
+
+
+print (3)
+
+
+# In[4]:
+
+
+3+3
+
+
+# In[8]:
+
+
+print (3+3)
+
+
+# In[9]:
+
+
+print(6-4)
+
+
+# In[10]:
+
+
+print (4*5)
+
+
+# In[14]:
+
+
+print (20/5)
+
+
+# In[17]:
+
+
+import numpy as np
+a = np.zeros (5)
+print (a)
+
+
+# In[20]:
+
+
+import numpy as np
+a = np.ones(5)
+print (a)
+
+
+# In[22]:
+
+
+import pandas as pd
+dt = pd.read_csv ('1000_Companies.csv')
+
+
+# In[33]:
+
+
+dt.head (1000)
+
+
+# In[ ]:
+
+
+
+
