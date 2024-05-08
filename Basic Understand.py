6765afa49a34620c65e3167a10cd00c315ac4776
DataVizGenius <168847652+DataVizGenius@users.noreply.github.com>
2024-05-08 22:33:17 +0530

    Add files via upload

---
+++ Basic Understand.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+if a>0:
+    print ('positive')
+else:
+    print ('negative')
+    
+
+
+# In[2]:
+
+
+print (5+2)
+
+
+# In[3]:
+
+
+b= 'positive'
+
+
+# In[4]:
+
+
+b
+
+
+# In[5]:
+
+
+type(a)
+
+
+# In[6]:
+
+
+type(b)
+
+
+# In[7]:
+
+
+C=5.4
+
+
+# In[9]:
+
+
+type (C)
+
+
+# In[10]:
+
+
+name=input()
+
+
+# In[11]:
+
+
+print (name)
+
+
+# In[13]:
+
+
+a= 10
+input ('enter value of a')
+
+
+# In[ ]:
+
+
+
+
